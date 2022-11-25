4497462f80514dcad9ae23605cec62c451c9629e
SchneiderSix <https://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@github.com>
2022-11-25 14:47:18 -0300

    old back

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -31,29 +31,21 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-        classes = {
-                    'State': State, 'User': User, 'Place': Place,
-                    'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
-        final_dictionary = {}
+        """All function
+        all objects depending of the class name
+        return a dictionary"""
         if cls:
-            for object in self.__session.query(classes[str(cls)]):
-                key_auxiliar = str(cls) + '.' + object.id
-                final_dictionary[key_auxiliar] = object.to_dict()
+            if type(cls) == str:
+                cls = eval(cls)
+            objs = self.__session.query(cls)
         else:
-            for class_aux in classes.keys():
-                for object in self.__session.query(classes[class_aux]):
-                    key_auxiliar = str(class_aux) + '.' + object.id
-                    final_dictionary[key_auxiliar] = object.to_dict()
-                    #print(final_dictionary)
-        return final_dictionary
+            objs = self.__session.query(State).all()
+            objs.extend(self.__session.query(City).all())
+            objs.extend(self.__session.query(User).all())
+            objs.extend(self.__session.query(Place).all())
+            objs.extend(self.__session.query(Review).all())
+            objs.extend(self.__session.query(Amenity).all())
+        return {"{}.{}".format(type(o).__name__, o.id): o for o in objs} 
 
     def new(self, obj):
         """Add obj to session"""

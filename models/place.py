b74ed0e9850217d8ca2b070c135a2b51610081f7
SchneiderSix <https://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@github.com>
2022-11-25 16:30:37 -0300

    Update place

--- models/place.py
+++ models/place.py
@@ -7,8 +7,8 @@ from models.review import Review
 from models.amenity import Amenity
 
 
-place_amenity = Table(
-    "place_amenities",
+association_table = Table(
+    "place_amenity",
     Base.metadata,
     Column("place_id", String(60),ForeignKey("places.id"), primary_key=True, nullable=False),
     Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False),

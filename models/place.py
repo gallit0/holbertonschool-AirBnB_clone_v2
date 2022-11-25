55f6c11823505fe769600b6a7fca193a48b371cd
Damian Caetano <5100@holbertonschool.com>
2022-11-25 12:28:59 -0800

    Fix place pycode

--- models/place.py
+++ models/place.py
@@ -11,8 +11,7 @@ association_table = Table(
     "place_amenity",
     Base.metadata,
     Column("place_id", String(60),ForeignKey("places.id"), primary_key=True, nullable=False),
-    Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False),
-)
+    Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False))
 
 class Place(BaseModel, Base):
     """ A place to stay """

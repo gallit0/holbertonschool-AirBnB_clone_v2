c2757aa9cb3972673542b846aaa7f8f5896e1f16
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-25 19:51:17 -0300

    Implement ondelete cascade&onupdate cascade pl_am

--- models/place.py
+++ models/place.py
@@ -10,8 +10,8 @@ from models.amenity import Amenity
 association_table = Table(
     "place_amenity",
     Base.metadata,
-    Column("place_id", String(60),ForeignKey("places.id"), primary_key=True, nullable=False),
-    Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False))
+    Column("place_id", String(60),ForeignKey("places.id"), primary_key=True, onupdate='CASCADE', ondelete='CASCADE', nullable=False),
+    Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, onupdate='CASCADE', ondelete='CASCADE', nullable=False))
 
 class Place(BaseModel, Base):
     """ A place to stay """

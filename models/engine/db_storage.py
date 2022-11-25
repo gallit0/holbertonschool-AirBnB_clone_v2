5e2470cfa8664b34c42979c152035834c30dd16d
SchneiderSix <https://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@github.com>
2022-11-25 10:53:06 -0300

    Break checker

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -36,7 +36,7 @@ class DBStorage:
         return a dictionary"""
         if cls is None:
             objs = self.__session.query(State).all()
-            #objs.extend(self.__session.query(City).all())
+            objs.extend(self.__session.query(City).all())
             #objs.extend(self.__session.query(User).all())
             #objs.extend(self.__session.query(Place).all())
             #objs.extend(self.__session.query(Review).all())

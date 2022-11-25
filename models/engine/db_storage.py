c998f1a184cceadd480cd257a496bd43d78a7fdb
SchneiderSix <https://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@github.com>
2022-11-25 15:29:39 -0300

    Update db_storage imports

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,14 +4,6 @@ Module db_storage
 """
 from sqlalchemy import create_engine
 from os import getenv
-from models.base_model import Base
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
 
@@ -34,6 +26,14 @@ class DBStorage:
         """All function
         all objects depending of the class name
         return a dictionary"""
+        from models.base_model import Base
+        from models.base_model import BaseModel
+        from models.amenity import Amenity
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.state import State
+        from models.user import User
         if cls:
             if type(cls) == str:
                 cls = eval(cls)

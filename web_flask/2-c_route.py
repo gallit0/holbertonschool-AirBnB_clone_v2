4315cb3d22f7ebcdacc0800ffae8298a26b347b8
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-28 16:43:13 -0300

    2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module c_route
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb_hello():
+        return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_route(text):
+       text.replace('_', ' ')
+       return "C " + text
+       
+
+
+if __name__ == '__main__':
+        app.run(host=('0.0.0.0'),
+                port=int('5000'), threaded=True)

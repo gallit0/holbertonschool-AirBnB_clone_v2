f7aadc4520204b138bfc579dc984c40d39b8b487
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-28 18:48:08 -0300

    5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,7 +39,7 @@ def number_route(n):
 
 @app.route("/number_template/<int:n>")
 def number_template_route(n):
-       return render_template("5-number.html", number = n)
+        return render_template("5-number.html", number=n)
 
 
 if __name__ == '__main__':

02ad5cef224ce809c6e7f315f3801a7029bfac08
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-28 16:15:38 -0300

    Update task0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,8 +6,13 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 @app.route("/")
-def hello_world():
+def hello_hbnb():
     return "Hello HBNB!"
+
+@app.route("/hbnb")
+def hbnb_hello():
+     return "HBNB"
+
 
 if __name__ == '__main__':
      app.run(host=('0.0.0.0'),

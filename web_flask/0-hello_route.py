68b93b8f02adf631ebbf07088bbeabdc97b0dcb7
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-28 16:19:39 -0300

    Fix syntax for task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,15 +8,17 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route("/")
 def hello_hbnb():
     return "Hello HBNB!"
 
+
 @app.route("/hbnb")
 def hbnb_hello():
-     return "HBNB"
+        return "HBNB"
 
 
 if __name__ == '__main__':
      app.run(host=('0.0.0.0'),
-            port=int('5000'), threaded=True)
+             port=int('5000'), threaded=True)

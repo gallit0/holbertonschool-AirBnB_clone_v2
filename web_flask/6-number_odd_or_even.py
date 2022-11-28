fee101bd6caa289bd12399e344b3e1396b37463a
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-28 19:08:04 -0300

    6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -16,37 +16,37 @@ def hello_hbnb():
 
 @app.route("/hbnb")
 def hbnb_hello():
-	return "HBNB"
+        return "HBNB"
 
 
 @app.route("/c/<text>")
 def c_route(text):
-	res = text.replace('_', ' ')
-	return "C " + res
+        res = text.replace('_', ' ')
+        return "C " + res
 
 
 @app.route("/python")
 @app.route("/python/<text>")
 def python_route(text="is cool"):
-	res = text.replace('_', ' ')
-	return "Python " + res
+        res = text.replace('_', ' ')
+        return "Python " + res
 
 
 @app.route("/number/<int:n>")
 def number_route(n):
-	return "{} is a number".format(n)
+        return "{} is a number".format(n)
 
 
 @app.route("/number_template/<int:n>")
 def number_template_route(n):
-	return render_template("5-number.html", number=n)
+        return render_template("5-number.html", number=n)
 
 
 @app.route("/number_odd_or_even/<int:n>")
 def number_odd_or_even_route(n):
-	return render_template("6-number_odd_or_even.html", number=n)
+        return render_template("6-number_odd_or_even.html", number=n)
 
 
 if __name__ == '__main__':
-	app.run(host=('0.0.0.0'),
-		port=int('5000'), threaded=True)
+        app.run(host=('0.0.0.0'),
+                port=int('5000'), threaded=True)

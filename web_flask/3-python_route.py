b08c956886410b02c32cf0cc2a1824f0bfe6211a
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-28 17:07:28 -0300

    3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Module python_route
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb_hello():
+        return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_route(text):
+        res = text.replace('_', ' ')
+        return "C " + res
+
+
+@app.route("/python/<text>")
+def python_route(text="is cool"):
+        if text:
+                res = text.replace('_', ' ')
+        return "Python " + res
+
+
+if __name__ == '__main__':
+        app.run(host=('0.0.0.0'),
+                port=int('5000'), threaded=True)

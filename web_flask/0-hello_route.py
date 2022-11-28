00e730b798a86749ad0de598becacf32b38cd1c3
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-28 16:08:31 -0300

    Fix syntax for task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,7 +5,7 @@ app.url_map.strict_slashes = False
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello HBNB!</p>"
+    return "Hello HBNB!"
 
 if __name__ == '__main__':
      app.run(host=('0.0.0.0'),

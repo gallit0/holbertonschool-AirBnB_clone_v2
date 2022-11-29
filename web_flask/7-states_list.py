852e5f32f158e2ddd6df143033776ad50d148ad5
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-29 10:16:28 -0300

    8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module 5-number_odd_or_even
+"""
+from flask import Flask, render_template
+from models import storage
+from models import State
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def deletesql():
+        storage.close()
+
+
+@app.route("/states_list")
+def number_odd_or_even_route():
+        return render_template("7-states_list.html", storage.all(State))
+
+
+if __name__ == '__main__':
+        app.run(host=('0.0.0.0'),
+                port=int('5000'), threaded=True)

50707cd6286cfbabbed95f00f2991ff22aa5bc54
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-29 12:05:24 -0300

    9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module 8-cities_by_states
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/states_list")
+def number_odd_or_even_route():
+        from models import storage
+        from models.state import State
+        from models.city import City
+        sts = storage.all(State)
+        cts = storage.all(City)
+        return render_template("7-states_list.html", sts=sts, cts=cts)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+        from models import storage
+        storage.close()
+
+
+if __name__ == '__main__':
+        app.run(host=('0.0.0.0'),
+                port=int('5000'), threaded=True)

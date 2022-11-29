d02bc97c46aa0727a2b63c60f048089717f2d2e8
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-29 11:20:57 -0300

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,8 +3,6 @@
 Module 7-states_list.py
 """
 from flask import Flask, render_template
-from models import storage
-from models.state import State
 
 
 app = Flask(__name__)
@@ -13,12 +11,15 @@ app.url_map.strict_slashes = False
 
 @app.route("/states_list")
 def number_odd_or_even_route():
+        from models import storage
+        from models.state import State
         sts = storage.all(State)
         return render_template("7-states_list.html", sts=sts)
 
 
 @app.teardown_appcontext
 def deletesqlalch():
+        from models import storage
         storage.close()
 
 
